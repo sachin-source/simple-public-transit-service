4f397a47b1721a716a4bcecc17d5bfaf2c586fd9
meghana Naik <meghana_n@eklakshya.com>
2024-10-14 17:30:00 +0530

    change field names

--- App/services.py
+++ App/services.py
@@ -1,12 +1,13 @@
 from models import Schedule
+from typing import List
 
 async def get_next_schedules(origin_station_id: str, destination_station_id: str) -> List[Schedule]:
     # This is a temporary code
     # The actual code to find the schedules based on origin_station_id, destination_station_id goes here
     # Submitting the Dummy response now
     return [
-        Schedule(transit_mode="rail", eta_origin="2023-10-14 16:19:09", eta_destination="2023-10-14 17:19:09"),
-        Schedule(transit_mode="bus", eta_origin="2023-10-14 16:30:09", eta_destination="2023-10-14 17:00:09"),
+        Schedule(transit_mode="rail", origin="2023-10-14 16:19:09", destination="2023-10-14 17:19:09"),
+        Schedule(transit_mode="bus", origin="2023-10-14 16:30:09", destination="2023-10-14 17:00:09"),
     ]
 
 async def get_closest_station(coordinates) -> str:
