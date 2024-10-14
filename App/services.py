904278d761d1eeff9fa65727146937f45d4abfca
meghana Naik <meghana_n@eklakshya.com>
2024-10-14 13:10:16 +0530

    generic service to get next schedule

---
+++ App/services.py
@@ -0,0 +1,11 @@
+from models import Schedule
+
+async def get_next_schedules(origin_station_id: str, destination_station_id: str) -> List[Schedule]:
+    # This is a temporary code
+    # The actual code to find the schedules based on origin_station_id, destination_station_id goes here
+    # Submitting the Dummy response now
+    return [
+        Schedule(transit_mode="rail", eta_origin="2023-10-14 16:19:09", eta_destination="2023-10-14 17:19:09"),
+        Schedule(transit_mode="bus", eta_origin="2023-10-14 16:30:09", eta_destination="2023-10-14 17:00:09"),
+    ]
+
