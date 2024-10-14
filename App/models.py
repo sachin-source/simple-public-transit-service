cb202c494ecfecaa50ccfa26f50aeb3bb6931c77
meghana Naik <meghana_n@eklakshya.com>
2024-10-14 17:28:40 +0530

    code optimization

--- App/models.py
+++ App/models.py
@@ -3,9 +3,8 @@ from typing import List
 
 class Schedule(BaseModel):
     transit_mode: str
-    eta_origin: str
-    eta_destination: str
-
+    origin: str
+    destination: str
 
 class Coordinates(BaseModel):
     latitude: str
